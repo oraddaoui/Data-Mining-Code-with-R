e50cf3782ae52f0e386bceff74404a3896e455c2
Omar Raddaoui <34781369+oraddaoui@users.noreply.github.com>
2019-08-22 19:20:42 -0600

    First code

---
+++ RPART.R
@@ -0,0 +1,24 @@
+### Data ### 
+dat = read.csv("C://5300//HWK3DATA.CSV", header=F)
+n = nrow(dat);
+Y = as.matrix(dat[,1:4]); P = ncol(Y)
+G = dat[,5] # grouping variable #
+library(rpart)
+# Cross-Validation # # leave-1-out #
+dat = data.frame(Y,G); G.class = NULL
+for(i in 1:n) {
+dat.i = dat[i,]; dat.xi = dat[-i,];
+set.seed(23)
+fit = rpart(G~.,data=dat.xi,method="class") # rpart #
+G.lab = predict(fit,newdata=dat.i,type="class")[[1]]
+G.class = c(G.class,as.character(G.lab)) }
+ct.cv = table(G,G.class) # confusion #
+ct.cv
+   G.class
+# Error Rate #
+ct = ct.cv
+error.rate = 1-sum(diag(ct))/n
+c(error.rate=error.rate)
+
+
+
